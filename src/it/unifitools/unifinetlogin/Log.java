4b1e25bfaa162824bb2558ff6ffade465d9a5ac0
giuse <demon.2347@gmail.com>
2013-02-07 01:02:55 +0000

    Added logging history

--- src/it/unifitools/unifinetlogin/Log.java
+++ src/it/unifitools/unifinetlogin/Log.java
@@ -1,12 +1,18 @@
 package it.unifitools.unifinetlogin;
 
+import java.util.ArrayList;
+
 /**
  * 
  * TODO: net logging
  *
  */
 public class Log {
+	private static ArrayList<String> logs = new ArrayList<String>();
 	public static void i(String log){
 		System.out.println(log);
+		logs.add(log);
 	}
+	
+	
 }
